2b662628000817c641c5b8e0adbf14718d1c97cb
Josh McArthur <joshua.mcarthur@gmail.com>
2014-05-27 09:18:07 +1200

    Adjust default recipe to move jar file into same folder as helper script (necessary to run)

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,14 +9,23 @@
 
 include_recipe "java"
 
-ark 'apktool-helper' do
-  url 'https://android-apktool.googlecode.com/files/apktool-install-linux-r05-ibot.tar.bz2'
-end
-
 ark 'apktool' do
-  url 'https://android-apktool.googlecode.com/files/apktool1.5.2.tar.bz2'
+  url 'https://android-apktool.googlecode.com/files/apktool-install-linux-r05-ibot.tar.bz2'
+  has_binaries ['apktool']
+  creates "apktool"
+  prefix_root '/opt'
+  prefix_home '/opt'
 end
 
-link "/usr/local/bin/apktool" do
-  to "/usr/local/apktool-helper/apktool"
+ark 'apktool-jar' do
+  url 'https://android-apktool.googlecode.com/files/apktool1.5.2.tar.bz2'
+  creates "apktool.jar"
+  path '/opt/apktool/'
+  prefix_root '/opt'
+  prefix_home '/opt'
+end
+
+link '/opt/apktool/apktool.jar' do
+  to '/opt/apktool-jar/apktool.jar'
+  action :create
 end
