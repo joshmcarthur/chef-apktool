e479f6289cd45d7e7876859c6e75a8f3332232e7
Josh McArthur <joshua.mcarthur@gmail.com>
2014-05-27 09:33:56 +1200

    Add specs for default recipe

---
+++ spec/apktool/recipes/default_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'apktool::default' do
+  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+
+  it 'downloads and extracts APK helpers' do
+    expect(chef_run).to download_and_extract_package 'apktool'
+  end
+
+  it 'downloads and extracts APK JAR file' do
+    expect(chef_run).to download_and_extract_package 'apktool-jar'
+  end
+
+  it 'creates a symlink between the APK helpers and the JAR file' do
+    expect(chef_run).to create_link '/opt/apktool/apktool.jar'
+  end
+end
