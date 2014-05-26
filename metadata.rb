d01af126e7434ee49d6c4a2f324a5bcfc8f675df
Josh McArthur <joshua.mcarthur@gmail.com>
2014-05-27 10:31:34 +1200

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,12 @@ maintainer       'Joshua McArthur'
 maintainer_email 'joshua.mcarthur@gmail.com'
 license          'MIT License'
 description      'Installs/Configures apktool'
-long_description 'Installs/Configures apktool'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+%w( debian ubuntu centos redhat fedora mac_os_x ).each do |os|
+  supports os
+end
 
 depends "java"
 depends "ark"
