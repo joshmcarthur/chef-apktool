e7fb257297ce01d5472f942b8c84e25f6392c5c0
Josh McArthur <joshua.mcarthur@gmail.com>
2014-05-27 10:39:26 +1200

    Changed README, so version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'joshua.mcarthur@gmail.com'
 license          'MIT License'
 description      'Installs/Configures apktool'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 %w( debian ubuntu centos redhat fedora mac_os_x ).each do |os|
   supports os
