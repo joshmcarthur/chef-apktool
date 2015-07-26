0dfe245ca99c2b224276926ae4f4a89942650d5d
Josh McArthur <joshua.mcarthur@gmail.com>
2015-07-27 10:26:49 +1200

    Resolve spec failure due to changed arguments to remote_file block

--- spec/apktool/recipes/default_spec.rb
+++ spec/apktool/recipes/default_spec.rb
@@ -4,10 +4,10 @@ describe 'apktool::default' do
   let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
   it 'downloads APKTool helper script' do
-    expect(chef_run).to create_remote_file('/usr/local/bin/apktool').with(mode: 755)
+    expect(chef_run).to create_remote_file('/usr/local/bin/apktool').with(mode: '0755')
   end
 
   it 'downloads and extracts the APK JAR file' do
-    expect(chef_run).to create_remote_file('/usr/local/bin/apktool.jar').with(mode: 755)
+    expect(chef_run).to create_remote_file('/usr/local/bin/apktool.jar').with(mode: '0755')
   end
 end
