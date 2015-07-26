b5b784a16dcfc312a2ecfcb6f534ec7249825f65
Josh McArthur <joshua.mcarthur@gmail.com>
2015-07-27 10:27:39 +1200

    Tag new version for release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'joshua.mcarthur@gmail.com'
 license          'MIT License'
 description      'Installs/Configures apktool'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 %w( debian ubuntu centos redhat fedora mac_os_x ).each do |os|
   supports os
