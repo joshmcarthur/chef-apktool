e6057923576e805c9c2a7048415b9560bb0a65e9
Josh McArthur <joshua.mcarthur@gmail.com>
2015-07-27 11:02:37 +1200

    Assert that changing versions with attribute works as expected

--- spec/apktool/recipes/default_spec.rb
+++ spec/apktool/recipes/default_spec.rb
@@ -10,4 +10,13 @@ describe 'apktool::default' do
   it 'downloads and extracts the APK JAR file' do
     expect(chef_run).to create_remote_file('/usr/local/bin/apktool.jar').with(mode: '0755')
   end
+
+  it 'downloads and extracts the APK with a specific version' do
+    chef_run.node.set['apktool']['version'] = '2.0.0'
+    chef_run.converge(described_recipe)
+    
+    expect(chef_run).to create_remote_file('/usr/local/bin/apktool.jar').with(
+      source: "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.0.0.jar"
+    )
+  end
 end
